99b21b720a6967238f5f7c3b74c511630c9d5aae
gabikreal1 <growgabhd@gmail.com>
2023-09-13 14:58:35 +0100

    first ios release

--- lib/Screens/company_adder.dart
+++ lib/Screens/company_adder.dart
@@ -10,6 +10,6 @@ class CompanyAdder extends StatefulWidget {
 class _CompanyAdderState extends State<CompanyAdder> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
